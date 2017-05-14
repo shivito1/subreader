8bdc6f8ac6271c014da4d561f77cf9653c37d2dc
shivito1 <johnshivjo@gmail.com>
2017-05-14 19:38:00 -0400

    working but still has random issue
    Error loop for tts already started

--- Subreader4.py
+++ Subreader4.py
@@ -1,49 +1,82 @@
 import pysrt,pyttsx,time
-from threading import Timer
+from threading import Thread
 from Tkinter import Label,Tk
 from idlelib.idle_test.mock_tk import Event
 
 
 set1 = 0
 set2 = 0
-subs = pysrt.open(r"C:\Users\admin\Desktop\Python-files\Deskinfo\eternal-loveSUBS\Eternal.Love.E14.srt")
+subcount = 0 
+interupt = 0
 
-def speechafter(sub):
-    engine = pyttsx.init()
-    engine.setProperty('rate',200)
-    engine.say(sub.replace('[','').replace(']','').replace('\'','').replace('"','').replace('<i>','').replace('</i>',''))
-    
-    engine.runAndWait()
+settozero = 0
+
+subs = pysrt.open(r"C:\Users\admin\Desktop\Python-files\Deskinfo\eternal-loveSUBS\Eternal.Love.E14.srt")
+compsrt = []
+
+def speechafter(subit,x):
+    try:
+        engine = pyttsx.init()
+        engine.setProperty('rate',x)
+        engine.say(subit.replace('[','').replace(']','').replace('\'','').replace('"','').replace('<i>','').replace('</i>',''))
+        engine.runAndWait()
+    except RuntimeError:
+        print str(set2) + 'FAILD here\n' + subit[:10] + '\n'
 def get_ms(t): 
     return (t.hours*60*60 + t.minutes*60 + t.seconds)*1000 + t.milliseconds 
 
 #start speaking
 def start_speak1(event):
-    engine = pyttsx.init()
-    engine.setProperty('rate',200)
-    engine.say('start')
     
-    engine.runAndWait()
-    start_speak()    
+    Thread(target=start_speak).start()
+    
+def curset2(val):
+    global set2
+    set2 = val
+    
     
 def start_speak():
-    set2 = 0
-
-    for sub in subs:
-        start = get_ms(subs[set2].start)
-        start2 = get_ms(subs[(set2 - 1)].start)
-        if set2 == 0:
-            newtime = get_ms(sub.start)
+    global set2
+    try:
+        inter
+        if inter == 'asdf':
+            pass
         else:
-            newtime = start - start2
-            print newtime / 1000
-
+            set2 = subcount
+    except:
+        set2 = subcount
 
         
-        set2 += 1
-        time.sleep(newtime / 1000)
-        speechafter(sub.text)
 
+    for item in range(len(subs)):
+        if interupt == 5:
+            while interupt == 5:
+                pass
+        else:
+            start = get_ms(subs[set2].start)
+            start2 = get_ms(subs[(set2 - 1)].start)
+            if set2 == 0:
+                newtime = get_ms(subs[0].start)
+                x = 200
+            else:
+                newtime = start - start2  
+                print float(newtime) / 1000 
+                if subs[set2].text.endswith(' zz$'):
+                    x=250
+                    subs[set2].text = subs[set2].text.replace(' zz$', '') 
+                else:
+                    x = 200
+            curset2(set2)
+            global settozero
+            if settozero == 5:
+                newtime = 0
+                settozero = 0
+            else:
+                pass
+
+            time.sleep(float(newtime) / 1000)
+            Thread(target=speechafter,args=(subs[set2].text, x)).start()
+            set2 += 1
         
 
         
@@ -53,21 +86,30 @@ for sub in subs:
     start = get_ms(sub.start)
 
     char = len(sub.text)
-    time_need = int((char / 3.0) / (200.0/60.0) * 1000.0)
-    
+    time_need = int((char / 3.0) / (200.0/60.0) * 1000.0) 
     if (sub.end - sub.start) > int(time_need):
-            pass
-    else:
-        subs[set1].end = (subs[set1].start + time_need)
-    try:
-        if subs[set1].end >= subs[(set1 +1)].start:
-            subs[(set1 +1)].start = (subs[set1].end)
-        else:
-            pass
-    except IndexError:
         pass
-    
+    else:
+        time_need = int((char / 3.0) / (250.0/60.0) * 1000.0)
+        subs[set1].end = (subs[set1].start + time_need)
+        subs[set1].text = subs[set1].text + ' zz$'
+        try:
+            if subs[set1].end >= subs[(set1 +1)].start:
+                subs[(set1 +1)].start = (subs[set1].end)
+            
+            else:
+                pass
+        except IndexError:
+            pass
     set1 +=1
+    compsrt.append(str(set1) + '\n' + str(sub.start) + " - " + str(sub.end) + '\n' + str(sub.text) + '\n\n')
+    
+    
+    
+ed = open('compsrt.txt', 'w')
+for i in compsrt:
+    ed.write(i)
+ed.close()
             
             
             
@@ -86,9 +128,37 @@ for sub in subs:
 #                    pass
 root = Tk()
 
+def nextline(event):
+    global inter
+    inter = 'asdf'
+    global set2
+    set2 = set2 + 1
+    print set2
+    print subs[set2].text
+def pause(event):
+    global settozero
+    settozero = 5
+    global interupt
+    if interupt == 5:
+        interupt = 0
+    else:
+        interupt = 5
+def backline(event):
+    global inter
+    inter = 'asdf'
+    global set2
+    set2 = set2 - 1
+    print subs[set2].text
+
+root.attributes("-topmost", True)
+
 lbl = Label(text="start")
 lbl.pack()
 lbl.bind("<Button-1>",start_speak1)
+root.bind('<Right>', nextline)
+root.bind('<Down>', pause)
+root.bind('<Left>', backline)
 root.mainloop()
  
     
+
